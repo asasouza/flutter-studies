e60e9861b50f00bd96143dc4829c47096b2ff3a4
Alex <asasouza.alex@gmail.com>
2020-01-30 21:39:11 -0300

    Class 232 - 239

--- flutter_state_shop/lib/models/products.dart
+++ flutter_state_shop/lib/models/products.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -47,20 +50,34 @@ class Products with ChangeNotifier {
   }
 
   void addNewItem(Product product) {
-    var newProduct = Product(
-      id: DateTime.now().toString(),
-      description: product.description,
-      imageUrl: product.imageUrl,
-      title: product.title,
-      price: product.price,
-    );
-    _items.add(newProduct);
-    notifyListeners();
+    const url = 'https://flutter-studies-5fc09.firebaseio.com/products.json';
+    http
+        .post(
+      url,
+      body: json.encode({
+        'description': product.description,
+        'imageUrl': product.imageUrl,
+        'title': product.title,
+        'price': product.price,
+        'isFavorite': product.isFavorite,
+      }),
+    )
+        .then((response) {
+      var newProduct = Product(
+        id: json.decode(response.body)['name'],
+        description: product.description,
+        imageUrl: product.imageUrl,
+        title: product.title,
+        price: product.price,
+      );
+      _items.add(newProduct);
+      notifyListeners();
+    });
   }
 
   void updateItem(String id, Product product) {
     final index = _items.indexWhere((product) => product.id == id);
-    if(index >= 0) {
+    if (index >= 0) {
       _items[index] = product;
       notifyListeners();
     }
